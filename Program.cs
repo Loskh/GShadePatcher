9f23dc07a316ffb22d7a9b329c01e0e77de1363e
Loskh <1020612624@qq.com>
2023-01-31 23:30:41 +0800

    Init

---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿using System.Runtime.InteropServices;
+using GShadePatcher;
+using Iced.Intel;
+using System.Reflection;
+using System.Linq.Expressions;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System;
+
+namespace GShadePatcher {
+    internal class Program {
+
+        //const string SkipUpdateSig = "74 ?? 48 8D 8F ?? ?? ?? ?? E8 ?? ?? ?? ?? 88 87";
+        const string SkipUpdateSig = "74 ?? 48 8B 15 ?? ?? ?? ?? 48 8D 4F";
+        [STAThread]
+        static void Main(string[] args) {
+            Console.WriteLine($"补丁版本: {Assembly.GetExecutingAssembly().GetName().Version}");
+            try {
+                var gamePath = TryGamePaths();
+                var ofd = new OpenFileDialog();
+                if (gamePath != null) {
+                    ofd.InitialDirectory = gamePath;
+                }
+                ofd.Multiselect = false;
+                ofd.CheckFileExists = true;
+                ofd.Title = "选择要进行Patch的GShade64";
+                ofd.Filter = "GShade64 dll file|*.dll";
+                var gshadeDllPath = string.Empty;
+                if (ofd.ShowDialog() == DialogResult.OK) {
+                    gshadeDllPath = ofd.FileName;
+                }
+                else {
+                    throw new Exception("请选择GShade64.dll或者ffxiv_dx11.exe所在文件夹下的dxgi.dll或者d3d11.dll");
+                }
+                gshadeDllPath = gshadeDllPath.Trim();
+                Console.WriteLine($"选择DLL路径:{gshadeDllPath}");
+                if (File.ResolveLinkTarget(gshadeDllPath, true) != null) {
+                    gshadeDllPath = File.ResolveLinkTarget(gshadeDllPath, true).FullName;
+                }
+                var gshadeDllVersion = FileVersionInfo.GetVersionInfo(gshadeDllPath);
+                Console.WriteLine(gshadeDllVersion);
+                var file = File.ReadAllBytes(gshadeDllPath);
+                var module = Marshal.AllocHGlobal(file.Length);
+                Marshal.Copy(file, 0, module, file.Length);
+                var scanner = new SigScanner(module);
+                Console.WriteLine($"Current Sig: {SkipUpdateSig}");
+                if (scanner.TryScanText(SkipUpdateSig, out var ptr)) {
+                    var offset = (int)((long)ptr - (long)scanner.SearchBase);
+                    Console.WriteLine($"Find: 0x{offset:X8}");
+
+                    var disasmNum = 32;
+                    var bytes = new byte[disasmNum];
+                    Console.WriteLine("Patch前:");
+                    Marshal.Copy(module + offset, bytes, 0, disasmNum);
+                    Disassemble(bytes, 0x180000C00 + (ulong)offset);
+
+                    Marshal.WriteByte(ptr, 0xEB);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Patch后:");
+                    Marshal.Copy(module + offset, bytes, 0, disasmNum);
+                    Disassemble(bytes, 0x180000C00 + (ulong)offset);
+
+                    Console.WriteLine();
+
+                    File.Copy(gshadeDllPath, $"{gshadeDllPath}.bak", true);
+                    File.Delete(gshadeDllPath);
+                    Marshal.Copy(module, file, 0, file.Length);
+                    File.WriteAllBytes(gshadeDllPath, file);
+                    Console.WriteLine("Patch成功");
+                }
+                else {
+                    throw new Exception("Can't find any referenced address!");
+                }
+            }
+            catch (UnauthorizedAccessException ex) {
+                Console.WriteLine();
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("无权写入，请尝试退出程序后右键管理员运行。");
+            }
+            catch (Exception ex) {
+                Console.WriteLine();
+                Console.WriteLine($"{ex.GetType().FullName}:{ex.Message}");
+                Console.WriteLine("反馈地址: https://nga.178.com/read.php?tid=26796541");
+                Console.WriteLine("请带上日志或者截图,反馈前请尝试原贴中最新版是否可用。");
+            }
+
+            Console.WriteLine("按任意键继续...");
+            Console.ReadKey();
+        }
+
+        public static string? TryGamePaths() {
+            foreach (var registryView in new RegistryView[] { RegistryView.Registry32, RegistryView.Registry64 }) {
+                using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView)) {
+                    using (var subkey = hklm.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{2B41E132-07DF-4925-A3D3-F2D1765CCDFE}")) {
+                        if (subkey != null && subkey.GetValue("DisplayIcon", null) is string path) {
+                            // DisplayIcon includes "boot\ffxivboot.exe", need to remove it
+                            path = Directory.GetParent(path).FullName;
+                            if (Directory.Exists(path)) {
+                                return path;
+                            }
+                        }
+                    }
+                    using (var subkey = hklm.OpenSubKey($@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\最终幻想14")) {
+                        if (subkey != null && subkey.GetValue("InstallLocation", null) is string path) {
+                            if (Directory.Exists(path)) {
+                                return path;
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        public static void Disassemble(byte[] codeBytes, ulong address) {
+            var codeReader = new ByteArrayCodeReader(codeBytes);
+            var decoder = Decoder.Create(64, codeReader);
+            decoder.IP = address;
+
+            var formatter = new MasmFormatter();
+            var output = new FormatterOutputImpl();
+            foreach (var instr in decoder) {
+                output.List.Clear();
+                formatter.Format(instr, output);
+                Console.Write(instr.IP.ToString("X16"));
+                Console.Write(" ");
+                Console.Write($"{BitConverter.ToString(codeBytes[(int)(instr.IP - address)..(int)(instr.IP - address + (ulong)instr.Length)]).Replace('-', ' '),-34}");
+                foreach (var (text, kind) in output.List) {
+                    Console.ForegroundColor = GetColor(kind);
+                    Console.Write(text);
+                }
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+        }
+
+        sealed class FormatterOutputImpl : FormatterOutput {
+            public readonly List<(string text, FormatterTextKind kind)> List =
+                new List<(string text, FormatterTextKind kind)>();
+            public override void Write(string text, FormatterTextKind kind) => List.Add((text, kind));
+        }
+
+        static ConsoleColor GetColor(FormatterTextKind kind) {
+            switch (kind) {
+                case FormatterTextKind.Directive:
+                case FormatterTextKind.Keyword:
+                    return ConsoleColor.Yellow;
+
+                case FormatterTextKind.Prefix:
+                case FormatterTextKind.Mnemonic:
+                    return ConsoleColor.Red;
+
+                case FormatterTextKind.Register:
+                    return ConsoleColor.Magenta;
+
+                case FormatterTextKind.Number:
+                    return ConsoleColor.Green;
+
+                default:
+                    return ConsoleColor.White;
+            }
+        }
+    }
+}
---
+++ SigScanner.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Iced.Intel;
+namespace GShadePatcher;
+// Copy from https://github.com/goatcorp/Dalamud/blob/master/Dalamud/Game/SigScanner.cs
+/// <summary>
+/// A SigScanner facilitates searching for memory signatures in a given ProcessModule.
+/// </summary>
+
+public class SigScanner : IDisposable {
+    public SigScanner(IntPtr module) {
+
+        // Limit the search space to .text section.
+        this.SetupSearchSpace(module);
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether or not the ProcessModule is 32-bit.
+    /// </summary>
+    public bool Is32BitProcess { get; }
+
+    /// <summary>
+    /// Gets the base address of the search area. When copied, this will be the address of the copy.
+    /// </summary>
+    public IntPtr SearchBase;
+
+    /// <summary>
+    /// Gets the base address of the .text section search area.
+    /// </summary>
+    public IntPtr TextSectionBase => new(this.SearchBase.ToInt64() + this.TextSectionOffset);
+
+    /// <summary>
+    /// Gets the offset of the .text section from the base of the module.
+    /// </summary>
+    public long TextSectionOffset { get; private set; }
+
+    /// <summary>
+    /// Gets the size of the text section.
+    /// </summary>
+    public int TextSectionSize { get; private set; }
+
+    /// <summary>
+    /// Gets the base address of the .data section search area.
+    /// </summary>
+    public IntPtr DataSectionBase => new(this.SearchBase.ToInt64() + this.DataSectionOffset);
+
+    /// <summary>
+    /// Gets the offset of the .data section from the base of the module.
+    /// </summary>
+    public long DataSectionOffset { get; private set; }
+
+    /// <summary>
+    /// Gets the size of the .data section.
+    /// </summary>
+    public int DataSectionSize { get; private set; }
+
+    /// <summary>
+    /// Gets the base address of the .rdata section search area.
+    /// </summary>
+    public IntPtr RDataSectionBase => new(this.SearchBase.ToInt64() + this.RDataSectionOffset);
+
+    /// <summary>
+    /// Gets the offset of the .rdata section from the base of the module.
+    /// </summary>
+    public long RDataSectionOffset { get; private set; }
+
+    /// <summary>
+    /// Gets the size of the .rdata section.
+    /// </summary>
+    public int RDataSectionSize { get; private set; }
+
+    private IntPtr TextSectionTop => this.TextSectionBase + this.TextSectionSize;
+
+    /// <summary>
+    /// Scan memory for a signature.
+    /// </summary>
+    /// <param name="baseAddress">The base address to scan from.</param>
+    /// <param name="size">The amount of bytes to scan.</param>
+    /// <param name="signature">The signature to search for.</param>
+    /// <returns>The found offset.</returns>
+    public static IntPtr Scan(IntPtr baseAddress, int size, string signature) {
+        var (needle, mask) = ParseSignature(signature);
+        var index = IndexOf(baseAddress, size, needle, mask);
+        if (index < 0)
+            throw new KeyNotFoundException($"Can't find a signature of {signature}");
+        return baseAddress + index;
+    }
+
+    /// <summary>
+    /// Try scanning memory for a signature.
+    /// </summary>
+    /// <param name="baseAddress">The base address to scan from.</param>
+    /// <param name="size">The amount of bytes to scan.</param>
+    /// <param name="signature">The signature to search for.</param>
+    /// <param name="result">The offset, if found.</param>
+    /// <returns>true if the signature was found.</returns>
+    public static bool TryScan(IntPtr baseAddress, int size, string signature, out IntPtr result) {
+        try {
+            result = Scan(baseAddress, size, signature);
+            return true;
+        }
+        catch (KeyNotFoundException) {
+            result = IntPtr.Zero;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Scan for a .data address using a .text function.
+    /// This is intended to be used with IDA sigs.
+    /// Place your cursor on the line calling a static address, and create and IDA sig.
+    /// The signature and offset should not break through instruction boundaries.
+    /// </summary>
+    /// <param name="signature">The signature of the function using the data.</param>
+    /// <param name="offset">The offset from function start of the instruction using the data.</param>
+    /// <returns>An IntPtr to the static memory location.</returns>
+    public unsafe IntPtr GetStaticAddressFromSig(string signature, int offset = 0) {
+        var instructionAddress = (byte*)this.ScanText(signature);
+        instructionAddress += offset;
+
+        try {
+            var reader = new UnsafeCodeReader(instructionAddress, signature.Length + 8);
+            var decoder = Decoder.Create(64, reader, (ulong)instructionAddress, DecoderOptions.AMD);
+            while (reader.CanReadByte) {
+                var instruction = decoder.Decode();
+                if (instruction.IsInvalid) continue;
+                if (instruction.Op0Kind is OpKind.Memory || instruction.Op1Kind is OpKind.Memory) {
+                    return (IntPtr)instruction.MemoryDisplacement64;
+                }
+            }
+        }
+        catch {
+            // ignored
+        }
+
+        throw new KeyNotFoundException($"Can't find any referenced address in the given signature {signature}.");
+    }
+
+    /// <summary>
+    /// Try scanning for a .data address using a .text function.
+    /// This is intended to be used with IDA sigs.
+    /// Place your cursor on the line calling a static address, and create and IDA sig.
+    /// </summary>
+    /// <param name="signature">The signature of the function using the data.</param>
+    /// <param name="result">An IntPtr to the static memory location, if found.</param>
+    /// <param name="offset">The offset from function start of the instruction using the data.</param>
+    /// <returns>true if the signature was found.</returns>
+    public bool TryGetStaticAddressFromSig(string signature, out IntPtr result, int offset = 0) {
+        try {
+            result = this.GetStaticAddressFromSig(signature, offset);
+            return true;
+        }
+        catch (KeyNotFoundException) {
+            result = IntPtr.Zero;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Scan for a byte signature in the .data section.
+    /// </summary>
+    /// <param name="signature">The signature.</param>
+    /// <returns>The real offset of the found signature.</returns>
+    public IntPtr ScanData(string signature) {
+        var scanRet = Scan(this.DataSectionBase, this.DataSectionSize, signature);
+
+        return scanRet;
+    }
+
+    /// <summary>
+    /// Try scanning for a byte signature in the .data section.
+    /// </summary>
+    /// <param name="signature">The signature.</param>
+    /// <param name="result">The real offset of the signature, if found.</param>
+    /// <returns>true if the signature was found.</returns>
+    public bool TryScanData(string signature, out IntPtr result) {
+        try {
+            result = this.ScanData(signature);
+            return true;
+        }
+        catch (KeyNotFoundException) {
+            result = IntPtr.Zero;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Scan for a byte signature in the .text section.
+    /// </summary>
+    /// <param name="signature">The signature.</param>
+    /// <returns>The real offset of the found signature.</returns>
+    public IntPtr ScanText(string signature) {
+
+        var mBase = this.TextSectionBase;
+        var scanRet = Scan(mBase, this.TextSectionSize, signature);
+
+        var insnByte = Marshal.ReadByte(scanRet);
+
+        if (insnByte == 0xE8 || insnByte == 0xE9)
+            scanRet = ReadJmpCallSig(scanRet);
+
+        return scanRet;
+    }
+
+    /// <summary>
+    /// Try scanning for a byte signature in the .text section.
+    /// </summary>
+    /// <param name="signature">The signature.</param>
+    /// <param name="result">The real offset of the signature, if found.</param>
+    /// <returns>true if the signature was found.</returns>
+    public bool TryScanText(string signature, out IntPtr result) {
+        try {
+            result = this.ScanText(signature);
+            return true;
+        }
+        catch (KeyNotFoundException) {
+            result = IntPtr.Zero;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Free the memory of the copied module search area on object disposal, if applicable.
+    /// </summary>
+    public void Dispose() {
+        //Marshal.FreeHGlobal(this.moduleCopyPtr);
+    }
+
+    /// <summary>
+    /// Helper for ScanText to get the correct address for IDA sigs that mark the first JMP or CALL location.
+    /// </summary>
+    /// <param name="sigLocation">The address the JMP or CALL sig resolved to.</param>
+    /// <returns>The real offset of the signature.</returns>
+    private static IntPtr ReadJmpCallSig(IntPtr sigLocation) {
+        var jumpOffset = Marshal.ReadInt32(sigLocation, 1);
+        return IntPtr.Add(sigLocation, 5 + jumpOffset);
+    }
+
+    private static (byte[] Needle, bool[] Mask) ParseSignature(string signature) {
+        signature = signature.Replace(" ", string.Empty);
+        if (signature.Length % 2 != 0)
+            throw new ArgumentException("Signature without whitespaces must be divisible by two.", nameof(signature));
+
+        var needleLength = signature.Length / 2;
+        var needle = new byte[needleLength];
+        var mask = new bool[needleLength];
+        for (var i = 0; i < needleLength; i++) {
+            var hexString = signature.Substring(i * 2, 2);
+            if (hexString == "??" || hexString == "**") {
+                needle[i] = 0;
+                mask[i] = true;
+                continue;
+            }
+
+            needle[i] = byte.Parse(hexString, NumberStyles.AllowHexSpecifier);
+            mask[i] = false;
+        }
+
+        return (needle, mask);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static unsafe int IndexOf(IntPtr bufferPtr, int bufferLength, byte[] needle, bool[] mask) {
+        if (needle.Length > bufferLength) return -1;
+        var badShift = BuildBadCharTable(needle, mask);
+        var last = needle.Length - 1;
+        var offset = 0;
+        var maxoffset = bufferLength - needle.Length;
+        var buffer = (byte*)bufferPtr;
+
+        while (offset <= maxoffset) {
+            int position;
+            for (position = last; needle[position] == *(buffer + position + offset) || mask[position]; position--) {
+                if (position == 0)
+                    return offset;
+            }
+
+            offset += badShift[*(buffer + offset + last)];
+        }
+
+        return -1;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static int[] BuildBadCharTable(byte[] needle, bool[] mask) {
+        int idx;
+        var last = needle.Length - 1;
+        var badShift = new int[256];
+        for (idx = last; idx > 0 && !mask[idx]; --idx) {
+        }
+
+        var diff = last - idx;
+        if (diff == 0) diff = 1;
+
+        for (idx = 0; idx <= 255; ++idx)
+            badShift[idx] = diff;
+        for (idx = last - diff; idx < last; ++idx)
+            badShift[needle[idx]] = last - idx;
+        return badShift;
+    }
+
+    private void SetupSearchSpace(IntPtr module) {
+        var baseAddress = module;
+        this.SearchBase = baseAddress;
+        // We don't want to read all of IMAGE_DOS_HEADER or IMAGE_NT_HEADER stuff so we cheat here.
+        var ntNewOffset = Marshal.ReadInt32(baseAddress, 0x3C);
+        var ntHeader = baseAddress + ntNewOffset;
+
+        // IMAGE_NT_HEADER
+        var fileHeader = ntHeader + 4;
+        var numSections = Marshal.ReadInt16(ntHeader, 6);
+
+        // IMAGE_OPTIONAL_HEADER
+        var optionalHeader = fileHeader + 20;
+
+        IntPtr sectionHeader;
+        if (this.Is32BitProcess) // IMAGE_OPTIONAL_HEADER32
+            sectionHeader = optionalHeader + 224;
+        else // IMAGE_OPTIONAL_HEADER64
+            sectionHeader = optionalHeader + 240;
+
+        // IMAGE_SECTION_HEADER
+        var sectionCursor = sectionHeader;
+        for (var i = 0; i < numSections; i++) {
+            var sectionName = Marshal.ReadInt64(sectionCursor);
+
+            // .text
+            switch (sectionName) {
+                case 0x747865742E: // .text
+                    this.TextSectionOffset = Marshal.ReadInt32(sectionCursor, 12);
+                    this.TextSectionSize = Marshal.ReadInt32(sectionCursor, 8);
+                    break;
+                case 0x617461642E: // .data
+                    this.DataSectionOffset = Marshal.ReadInt32(sectionCursor, 12);
+                    this.DataSectionSize = Marshal.ReadInt32(sectionCursor, 8);
+                    break;
+                case 0x61746164722E: // .rdata
+                    this.RDataSectionOffset = Marshal.ReadInt32(sectionCursor, 12);
+                    this.RDataSectionSize = Marshal.ReadInt32(sectionCursor, 8);
+                    break;
+            }
+
+            sectionCursor += 40;
+        }
+    }
+
+
+    private unsafe class UnsafeCodeReader : CodeReader {
+        private readonly int length;
+        private readonly byte* address;
+        private int pos;
+
+        public UnsafeCodeReader(byte* address, int length) {
+            this.length = length;
+            this.address = address;
+        }
+
+        public bool CanReadByte => this.pos < this.length;
+
+        public override int ReadByte() {
+            if (this.pos >= this.length) return -1;
+            return *(this.address + this.pos++);
+        }
+    }
+}
+
+
